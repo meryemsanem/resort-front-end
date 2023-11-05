edbc2dca1b43a1b1110bbdb2fdf7c4306ae0679d
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-11-06 01:13:17 +0300

    Create route components

---
+++ src/components/Resort/AddResort.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AddResort() {
+  return (
+    <div>
+      <h1>Add Resort</h1>
+    </div>
+  );
+}
---
+++ src/components/Resort/Reserve.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Reserve() {
+  return (
+    <div>
+      <h1>Reserve</h1>
+    </div>
+  );
+}
