d1baf72b0a5028c7488ae1180f27948005bb5d73
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-10 10:14:11 +0200

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { FiMenu, FiX } from 'react-icons/fi';
 import {
   FaTwitter,
@@ -9,9 +10,12 @@ import {
   FaVimeoV,
   FaCopyright,
 } from 'react-icons/fa';
+import { logOut } from '../redux/AuthenticationSlice';
 import './Navigation.css';
 
 const Navigation = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
@@ -23,89 +27,3 @@ const Navigation = () => {
       setIsOpen(false);
     }
   };
-  useEffect(() => {
-    const handleResize = () => {
-      const isMobile = window.innerWidth <= 768;
-      if (!isMobile) {
-        setIsOpen(true);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  return (
-    <div className={`nav-menu ${isOpen ? 'open-menu' : 'close-menu'}`}>
-      <button type="button" onClick={handleToggle} className="hamburger-icon">
-        {isOpen ? <FiX className="close" /> : <FiMenu className="open" />}
-      </button>
-      {isOpen && (
-        <div className="menu-links">
-          <div className="logo-container">
-            <h2 className="app-name">Resort Vista</h2>
-          </div>
-          <div className="links">
-            <NavLink to="/" onClick={handleClick} activeClassName="active">
-              Resorts
-            </NavLink>
-            <NavLink
-              to="/reserve"
-              onClick={handleClick}
-              activeClassName="active"
-            >
-              Reserve
-            </NavLink>
-            <NavLink
-              to="/reservations"
-              onClick={handleClick}
-              activeClassName="active"
-            >
-              My Reservations
-            </NavLink>
-            <NavLink
-              to="/add_resort"
-              onClick={handleClick}
-              activeClassName="active"
-            >
-              Add Resort
-            </NavLink>
-            <NavLink
-              to="/delete_resort"
-              onClick={handleClick}
-              activeClassName="active"
-            >
-              Delete Resort
-            </NavLink>
-            <NavLink
-              to="/logout"
-              onClick={handleClick}
-              activeClassName="active"
-            >
-              Log out
-            </NavLink>
-          </div>
-          <div className="footer">
-            <div className="social-media">
-              <FaTwitter />
-              <FaFacebookF />
-              <FaGooglePlus />
-              <FaVimeoV />
-              <FaPinterestP />
-            </div>
-            <div className="copyright">
-              <FaCopyright />
-              <span>Copyright 2023</span>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Navigation;
