c12f3b369a8443c19007804247a22306b290568a
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-10 13:03:27 +0100

    Fetch data from API

--- src/components/Pages/Destinations.jsx
+++ src/components/Pages/Destinations.jsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-multi-carousel/lib/styles.css';
 import styles from './Destinations.css';
-
 const Destinations = () => {
-  const destinations = useSelector((state) => state.destinations);
+  const [destinations, setDestinations] = useState([]);
   const navigate = useNavigate();
   const {
-    card,
-    cardTop,
-    cardBottom,
-    carouselContainer,
-    carouselTitle,
+    card, cardTop, cardBottom, carouselContainer, carouselTitle,
   } = styles;
-
   const settings = {
     dots: true,
     infinite: false,
@@ -52,17 +45,37 @@ const Destinations = () => {
       },
     ],
   };
-
   const handleSpecificPage = (id) => {
     navigate(`/destinations/${id}`);
   };
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'http://127.0.0.1:4000/api/v1/destinations',
+        );
+        const data = await response.json();
+        setDestinations(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="page-container">
       <div className={carouselContainer}>
         <h1 className={carouselTitle}>LATEST SAFARIS</h1>
         {destinations && destinations.length > 0 ? (
-          <Slider {...settings}>
+          <Slider
+            dots={settings.dots}
+            infinite={settings.infinite}
+            speed={settings.speed}
+            slidesToShow={settings.slidesToShow}
+            slidesToScroll={settings.slidesToScroll}
+            initialSlide={settings.initialSlide}
+            responsive={settings.responsive}
+          >
             {destinations.map((destination) => (
               <div key={destination.id}>
                 <div className={card}>
@@ -73,7 +86,10 @@ const Destinations = () => {
                   <div className={cardBottom}>
                     <h3>{destination.name}</h3>
                     <p>{destination.description}</p>
-                    <button type="button" onClick={() => handleSpecificPage(destination.id)}>
+                    <button
+                      type="button"
+                      onClick={() => handleSpecificPage(destination.id)}
+                    >
                       Learn More
                     </button>
                   </div>
@@ -88,5 +104,4 @@ const Destinations = () => {
     </div>
   );
 };
-
 export default Destinations;
