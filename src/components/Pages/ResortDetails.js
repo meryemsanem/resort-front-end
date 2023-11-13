585f073612068e291c2cbc03309eeb9bff7a5b1f
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-13 21:51:00 +0200

    Update ResortDetails.js

--- src/components/Pages/ResortDetails.js
+++ src/components/Pages/ResortDetails.js
@@ -9,76 +9,80 @@ const ResortDetails = () => {
   const { state } = useLocation();
   const navigate = useNavigate();
   const [fee, setFee] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { destination } = state;
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
           `http://127.0.0.1:4000/api/v1/destinations/${destination.id}`,
         );
-
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         const data = await response.json();
         setFee(data.fee);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
       }
     };
-
     if (destination) {
       fetchData();
     }
   }, [destination]);
-
-  return destination ? (
+  const handleReserveClick = () => {
+    navigate('/reserve', { state: { destination } });
+  };
+  return (
     <IconContext.Provider value={{ color: '#fff' }}>
       <section id="detail-container">
-        <div id="card-container">
-          <div id="img-container">
-            <img
-              src={destination.image_url}
-              alt={destination.name}
-              id="detail-img"
-            />
-          </div>
-          <div id="detail-info">
-            <div className="resort-address">
-              <h1 id="resort-name">{destination.name}</h1>
-              <p id="resort-description">{destination.description}</p>
+        {loading && <p>Loading...</p>}
+        {!loading && destination ? (
+          <div id="card-container">
+            <div id="img-container">
+              <img
+                src={destination.image_url}
+                alt={destination.name}
+                id="detail-img"
+              />
             </div>
-            <div className="price-container">
-              <div className="details">
-                <li>
-                  <p className="info-name">
-                    Fee:
-                    {fee}
-                  </p>
-                </li>
-                <li>
-                  <p className="info-name">
-                    Name:
-                    {destination.name}
-                  </p>
-                </li>
+            <div id="detail-info">
+              <div className="resort-address">
+                <h1 id="resort-name">{destination.name}</h1>
+                <p id="resort-description">{destination.description}</p>
               </div>
+              <div className="price-container">
+                <div className="details">
+                  <li>
+                    <p className="info-name">
+                      Fee:
+                      {fee}
+                    </p>
+                  </li>
+                  <li>
+                    <p className="info-name">
+                      Name:
+                      {destination.name}
+                    </p>
+                  </li>
+                </div>
+              </div>
+              <button
+                id="reserver-btn"
+                type="button"
+                onClick={handleReserveClick}
+              >
+                Reserve
+                {' '}
+                <FcIcon.FcOvertime id="reserve-icon" />
+              </button>
             </div>
-            <button
-              id="reserver-btn"
-              type="button"
-              onClick={() => {
-                navigate('/reservations');
-              }}
-            >
-              Reserve
-              {' '}
-              <FcIcon.FcOvertime id="reserve-icon" />
-            </button>
           </div>
-        </div>
+        ) : (
+          !loading && <p>Destination not available.</p>
+        )}
         <button
           type="button"
           className="back"
@@ -91,7 +95,6 @@ const ResortDetails = () => {
         </button>
       </section>
     </IconContext.Provider>
-  ) : null;
+  );
 };
-
 export default ResortDetails;
