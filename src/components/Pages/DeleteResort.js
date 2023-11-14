6f9f4e6abadb45c6189b0f042223f3e95e03e6d7
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-14 22:42:03 +0100

    Refine DeleteResort UI

--- src/components/Pages/DeleteResort.js
+++ src/components/Pages/DeleteResort.js
@@ -39,16 +39,14 @@ const DeleteResort = () => {
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>City</th>
-            <th>Action</th>
+            <th>Lists of Resorts</th>
+            <th>Delete Resorts</th>
           </tr>
         </thead>
         <tbody>
           {destinations.map((destination) => (
             <tr key={destination.id}>
               <td>{destination.name}</td>
-              <td>{destination.city}</td>
               <td>
                 <button
                   type="button"
