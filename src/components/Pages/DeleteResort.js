7abc3749f8c1b46e778e33a0e831bac2b5a0c763
clarence <tegumclarence@gmail.com>
2023-11-15 10:09:40 +0100

    modify delete items

--- src/components/Pages/DeleteResort.js
+++ src/components/Pages/DeleteResort.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function DeleteResort() {
   return (
+   
     <div>
-      <h1>Delete Resort</h1>
-    </div>
-  );
+    <h1>Delete Resort</h1>
+  </div>
+);
 }
