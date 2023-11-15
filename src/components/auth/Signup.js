142bdaabd87b6a317ba8d7f19868b39014ef04ad
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-15 17:11:28 +0200

    style login & sign up

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { signUp } from '../redux/AuthenticationSlice';
+import './Signup.css'; // Import the CSS file
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -44,44 +45,47 @@ const Signup = () => {
   };
   return (
     <>
-      <form onSubmit={handleSubmit} className="signup-form">
-        <h1>Welcome to Resort Vista!</h1>
-        <div>
-          <input
-            type="text"
-            name="name"
-            value={newUser.user.name}
-            onChange={handleChange}
-            placeholder="name"
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            value={newUser.user.email}
-            onChange={handleChange}
-            placeholder="email"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={newUser.user.password}
-            onChange={handleChange}
-            placeholder="password"
-            required
-          />
-          <small className={`error-message ${error ? 'visible' : 'hidden'}`}>
-            {errorMessage}
-          </small>
-          <span>
-            Already have an account?
-            {' '}
-            <Link to="/login">Log In and explore!</Link>
-          </span>
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
+      <div className="signup">
+        <form onSubmit={handleSubmit} className="signup-form">
+          <h1>Welcome to Resort Vista!</h1>
+          <div>
+            <input
+              type="text"
+              name="name"
+              value={newUser.user.name}
+              onChange={handleChange}
+              placeholder="name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              value={newUser.user.email}
+              onChange={handleChange}
+              placeholder="email"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              value={newUser.user.password}
+              onChange={handleChange}
+              placeholder="password"
+              required
+            />
+            <small className={`error-message ${error ? 'visible' : 'hidden'}`}>
+              {errorMessage}
+            </small>
+            <span>
+              Already have an account?
+              {' '}
+              <Link to="/login">Log In and explore!</Link>
+            </span>
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+
     </>
   );
 };
