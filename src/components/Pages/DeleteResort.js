f8e77555dbc72741229b046d5dbad73214102d1e
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-17 12:35:26 +0200

    semicolomn

--- src/components/Pages/DeleteResort.js
+++ src/components/Pages/DeleteResort.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './DeleteResort.css'
+import './DeleteResort.css';
 
 const DeleteResort = () => {
   const [destinations, setDestinations] = useState([]);
