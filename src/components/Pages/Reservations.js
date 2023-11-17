b51918bc465cb2e56dd7a5cd1f61c6a0b5da80ff
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-17 10:06:55 +0100

    Refine Reservations functionality

--- src/components/Pages/Reservations.js
+++ src/components/Pages/Reservations.js
@@ -10,45 +10,37 @@ const Reservations = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.authentication);
   const [errorMessage, setErrorMessage] = useState('');
-
   useEffect(() => {
     const fetchReservationsData = async (userId, authToken) => {
       try {
         setLoading(true);
-
         if (!authToken) {
           return;
         }
-
         const response = await fetch(
-          `https://resort-vista.onrender.com/api/v1/users/${userId}/reservations`,
+          `http://127.0.0.1:4000/api/v1/users/${userId}/reservations`,
           {
             headers: {
               Authorization: authToken,
             },
           },
         );
-
         if (!response.ok) {
           return;
         }
-
         const data = await response.json();
-
         const reservationsWithFee = await Promise.all(
           data.map(async (reservation) => {
             const feeResponse = await fetch(
-              `https://resort-vista.onrender.com/api/v1/destinations/${reservation.destination_id}`,
+              `http://127.0.0.1:4000/api/v1/destinations/${reservation.destination_id}`,
             );
             const feeData = await feeResponse.json();
-
             return {
               ...reservation,
               fee: feeData.fee,
             };
           }),
         );
-
         setReservations(reservationsWithFee);
       } catch (error) {
         setErrorMessage('Error fetching reservations.');
@@ -56,58 +48,52 @@ const Reservations = () => {
         setLoading(false);
       }
     };
-
-    dispatch(fetchCurrentUser())
-      .then(() => {
-        const authToken = sessionStorage.getItem('authToken');
-        const userId = currentUser.user_id;
-
-        fetchReservationsData(userId, authToken);
-      });
+    dispatch(fetchCurrentUser()).then(() => {
+      const authToken = sessionStorage.getItem('authToken');
+      const userId = currentUser.user_id;
+      fetchReservationsData(userId, authToken);
+    });
   }, [dispatch, currentUser]);
   return (
     <div className="reservations-container">
       <h1 className="reservations-title">Reservations</h1>
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
       {isLoading && <LoadingSpinner />}
       {!isLoading && (
         <>
           {reservations.length > 0 ? (
-            <table className="reservation-table">
-              <thead>
-                <tr>
-                  <th>Resort</th>
-                  <th>Fee</th>
-                  <th>Total Fee</th>
-                  <th>Start Date</th>
-                  <th>End Date</th>
-                  <th>City</th>
-                </tr>
-              </thead>
-              <tbody>
-                {reservations.map((reservation) => (
-                  <tr key={reservation.id}>
-                    <td className="destination-name">
-                      {reservation.destination.name}
-                    </td>
-                    <td>
-                      $
+            <div className="card-container2">
+              {reservations.map((reservation) => (
+                <div key={reservation.id} className="card2">
+                  <div className="card-info" id="card-info">
+                    <h2>{reservation.destination.name}</h2>
+                    <p>
+                      Fee: $
                       {reservation.fee}
-                    </td>
-                    <td>
-                      $
+                    </p>
+                    <p>
+                      Total Fee: $
                       {reservation.fee
                         * ((new Date(reservation.end_date)
                           - new Date(reservation.start_date))
                           / (1000 * 60 * 60 * 24))}
-                    </td>
-                    <td>{reservation.start_date}</td>
-                    <td>{reservation.end_date}</td>
-                    <td>{reservation.destination.city_name}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+                    </p>
+                    <p>
+                      Start Date:
+                      {reservation.start_date}
+                    </p>
+                    <p>
+                      End Date:
+                      {reservation.end_date}
+                    </p>
+                    <p>
+                      City:
+                      {reservation.destination.city_name}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           ) : (
             <p className="no-reservations-message">No reservations.</p>
           )}
@@ -116,5 +102,4 @@ const Reservations = () => {
     </div>
   );
 };
-
 export default Reservations;
